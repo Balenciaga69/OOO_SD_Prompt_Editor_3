7595764b4d6ae951ce470547fbd0ae9f7edb9295
Emilio "El Carnicero" Gonzalez <84565530+emilioCodigo@users.noreply.github.com>
2023-02-23 00:10:59 +0800

    feat: Add new TSX files, and routes (#1)
    * add files
    
    * add files
    
    * add layout

--- src/core/router.tsx
+++ src/core/router.tsx
@@ -1,15 +1,20 @@
-import { Layout } from '@/views/layout'
 import React, { FC } from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { Layout } from '@/views/layout'
+import { Error } from '@/views/pages/Error'
+import { EditorPrompt } from '@/views/pages/EditorPrompt'
+import { EditorArticle } from '@/views/pages/EditorArticle'
+import { Intro } from '@/views/pages/Intro'
 export const AppRoutes: FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path='error/*' element={<></>} />
-          <Route path='logout' element={<></>} />
-          <Route path='auth/*' element={<></>} />
-          <Route path='*' element={<Navigate to='/auth' />} />
+          <Route path='error/*' element={<Error />} />
+          <Route path='intro' element={<Intro />} />
+          <Route path='editor/prompt' element={<EditorPrompt />} />
+          <Route path='editor/article' element={<EditorArticle />} />
+          <Route path='*' element={<Navigate to='/error' />} />
         </Route>
       </Routes>
     </BrowserRouter>
