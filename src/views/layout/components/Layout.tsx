8164bf868b6ac887b71d709e20619332ff5885e0
Hex <zywzjsc@gmail.com>
2023-02-23 00:07:40 +0800

    add layout

--- src/views/layout/components/Layout.tsx
+++ src/views/layout/components/Layout.tsx
@@ -1,9 +1,35 @@
-import React, { FC } from 'react'
+import { AppShell, Navbar } from '@mantine/core'
+import React, { FC, ReactNode } from 'react'
 import { Outlet } from 'react-router-dom'
+interface Main {
+  main: { backgroundColor: string }
+}
+interface DemoItemProp {
+  children: ReactNode
+}
 export const Layout: FC = () => {
   return (
     <>
-      <Outlet />
+      <DemoItem>
+        <Outlet />
+      </DemoItem>
     </>
+  )
+}
+const DemoItem: FC<DemoItemProp> = (prop: DemoItemProp) => {
+  return (
+    <AppShell
+      padding='md'
+      navbar={
+        <Navbar width={{ base: 60 }} height='100%' p='xs'>
+          {/* Navbar content */}
+        </Navbar>
+      }
+      styles={(theme): Main => ({
+        main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+      })}
+    >
+      {prop.children}
+    </AppShell>
   )
 }
