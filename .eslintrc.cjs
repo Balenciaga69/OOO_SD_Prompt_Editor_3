616507872d1b8570c48a029e4cd1aacd7ecab16f
KeyValuePair <zywzjsc@gmail.com>
2023-04-04 13:11:23 +0800

    no message

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,28 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:lodash/recommended', 'plugin:jsx-a11y/recommended', 'plugin:react-hooks/recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:lodash/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
-      extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:lodash/recommended', 'plugin:jsx-a11y/recommended', 'plugin:react-hooks/recommended', 'plugin:promise/recommended', 'prettier'],
+      extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:lodash/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:promise/recommended',
+        'prettier',
+      ],
       rules: {
         'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': 'off',
